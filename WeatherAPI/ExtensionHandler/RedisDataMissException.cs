4e4e94eed91ce2c3c5b75ae12b2a97adfba6bb74
TigerSamurai <tigran.avetisyan97@gmail.con>
2026-05-19 21:29:44 +0400

    written the redis cache checking method and exception handler for missing data in redis

---
+++ WeatherAPI/ExtensionHandler/RedisDataMissException.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherAPI.Properties.ExtensionHandler;
+
+public class RedisDataMissException : Exception
+{
+    public RedisDataMissException(string cityNotInRedis)
+        : base("City not in cache")
+    {
+    }
+}
--- WeatherAPI/Service/WeatherService.cs
+++ WeatherAPI/Service/WeatherService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.Json;
 using Microsoft.Extensions.Caching.Distributed;
+using WeatherAPI.Properties.ExtensionHandler;
 
 namespace WeatherAPI.Properties;
 
@@ -7,11 +8,13 @@ public class WeatherService
 {
     private readonly HttpClient _httpClient;
     private readonly IDistributedCache _cache;
+    private readonly ILogger<WeatherService> _logger;
     
-    public WeatherService(HttpClient httpClient, IDistributedCache cache)
+    public WeatherService(HttpClient httpClient, IDistributedCache cache,  ILogger<WeatherService> logger)
     {
         _httpClient = httpClient;
         _cache = cache;
+        _logger = logger;
     }
 
     public async Task<WeatherSummary?>  CheckRedisForCity(string city)
@@ -26,9 +29,24 @@ public class WeatherService
             return null;
         }
 
-        WeatherSummary? foundCity = JsonSerializer.Deserialize<WeatherSummary>(CheckRedisForCityAsync); // TODO: make a try-catch block
+        WeatherSummary? foundCity = null;
+        
+        try
+        {
+            foundCity = JsonSerializer.Deserialize<WeatherSummary>(CheckRedisForCityAsync);
+        }
+        catch (JsonException ex)
+        {
+            _logger.LogWarning(ex, "Failed to show info");
+            return null;
+        }
         
         Console.WriteLine("Found in cache! Returning data...");
         return foundCity;
+    }
+
+    private async Task<RootObject?> getCityWeatherInfo(string city)
+    {
+        //TODO: write the method
     }
 }
